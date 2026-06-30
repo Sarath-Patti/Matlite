0a441bc30c740e25900f918c6b54428f4060f44e
Sarath Patti <sarathpatti@Saraths-MacBook-Air.local>
2026-06-30 17:23:14 +0530

    v0.1 Initial project structure and REPL

---
+++ build/CMakeFiles/matlite.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/matlite.dir/src/main.cpp.o"
+  "CMakeFiles/matlite.dir/src/main.cpp.o.d"
+  "matlite"
+  "matlite.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/matlite.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/matlite.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for matlite.
+# This may be replaced when dependencies are built.
